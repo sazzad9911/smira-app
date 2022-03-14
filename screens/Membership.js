72a776686852a2843d38dd891059ec74913c25fd
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-03-14 15:26:45 +0000

    membership done

--- screens/Membership.js
+++ screens/Membership.js
@@ -5,18 +5,14 @@ import { View, Text, ScrollView, Dimensions, TouchableOpacity, StyleSheet } from
 const Membership = () => {
     return (
         <View style={style.body}>
-            <Text style={{
-                fontSize: 20,
-                marginTop: 15,
-                marginBottom:10
-            }}>Claim Your <Text style={{
+            <Text style={style.bodyHeaderText}>Claim Your <Text style={{
                 color: '#FA454B'
             }}>Free Month</Text></Text>
             <ScrollView>
                 <View>
-                    <MembershipSlide></MembershipSlide>
-                    <MembershipSlide></MembershipSlide>
-                    <MembershipSlide></MembershipSlide>
+                    <MembershipSlide headcolor='#FA454B' night='40' hotel='5' account='3' amount='50000' Buttoncolor='#FA454B'></MembershipSlide>
+                    <MembershipSlide headcolor='#D4B931' night='50' hotel='6' account='4' amount='60000' Buttoncolor='#D4B931'></MembershipSlide>
+                    <MembershipSlide headcolor='#31D485' night='60' hotel='7' account='5' amount='70000' Buttoncolor='#31D485'></MembershipSlide>
                 </View>
             </ScrollView>
 
@@ -34,19 +30,21 @@ const Membership = () => {
 
 export default Membership;
 
-const MembershipSlide = () => {
+const MembershipSlide = (props) => {
     const { height, width } = Dimensions.get('screen');
     return (
         <View style={style.slideView}>
             <View style={style.slideContent}>
-                <Text style={style.slideContentHead}>Silver Membership</Text>
-                <Text style={style.textMargin}>Hotel stays of up to 40 nights</Text>
-                <Text style={style.textMargin}>Valid on any 5 hotels</Text>
-                <Text style={style.textMargin}>Family access upto 3 accounts</Text>
-                <Text style={style.textMargin}>Benefits worth of ₹50000</Text>
+                <Text style={style.slideContentHead}><Text style={{color:props.headcolor}}>Silver</Text> Membership</Text>
+                <Text style={style.textMargin}>Hotel stays of up to {props.night} nights</Text>
+                <Text style={style.textMargin}>Valid on any {props.hotel} hotels</Text>
+                <Text style={style.textMargin}>Family access upto {props.account} accounts</Text>
+                <Text style={style.textMargin}>Benefits worth of ₹{props.amount}</Text>
 
                 <TouchableOpacity>
-                    <View style={style.bottomButton}>
+                    <View style={[style.bottomButton,{
+                        backgroundColor: props.Buttoncolor
+                    }]}>
                         <Text style={{
                             color: 'white',
                             fontSize: 20,
@@ -81,15 +79,16 @@ const style = StyleSheet.create({
         shadowOffset: {
             width: 2,
             height: 2,
-          },
-          shadowOpacity:0.3,
-          shadowRadius:5,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        elevation:3
     },
-    slideContent:{
+    slideContent: {
         padding: '5%',
         alignItems: 'center',
     },
-    slideContentHead:{
+    slideContentHead: {
         fontSize: 25,
         marginBottom: 10
     },
@@ -98,7 +97,12 @@ const style = StyleSheet.create({
         alignItems: 'center',
         height: '100%'
     },
-    bottomButton:{
+    bodyHeaderText: {
+        fontSize: 20,
+        marginTop: 15,
+        marginBottom: 10
+    },
+    bottomButton: {
         backgroundColor: '#FA454B',
         alignItems: 'center',
         paddingLeft: '15%',
