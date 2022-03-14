b869310bedf9bb2c19c5468e5191cc7e13c22a9e
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-03-14 23:49:58 +0600

    booking history

---
+++ screens/DealCoupon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View ,Text} from 'react-native';
+
+const DealCoupon = () => {
+    return (
+        <View>
+            <Text>Deal Coupon</Text>
+        </View>
+    );
+};
+
+export default DealCoupon;
