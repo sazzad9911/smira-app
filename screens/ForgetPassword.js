80a32d908f7b2cc97540d782b881a5c8592859d4
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-16 00:31:12 +0600

    ConfirmMessage page Done

--- screens/ForgetPassword.js
+++ screens/ForgetPassword.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Image, ScrollView, Button, TouchableOpacity, Switch, TextInput, StyleSheet } from 'react-native'
 import RedeemHistory from './RedeemHistory';
+import { AntDesign } from '@expo/vector-icons';
 
 const ForgetPassword = () => {
     const [text, onChangeText] = React.useState(null);
@@ -8,16 +9,16 @@ const ForgetPassword = () => {
 
     return (
         <ScrollView>
-        {
-            Confirm? (
-                <ConfirmMessage/>
-            ):(
-                <GetInstruction value={text} 
-                onChange={onChangeText}
-                    setConfirm={setConfirm}
-                />
-            )
-        }
+            {
+                Confirm ? (
+                    <ConfirmMessage />
+                ) : (
+                    <GetInstruction value={text}
+                        onChange={onChangeText}
+                        setConfirm={setConfirm}
+                    />
+                )
+            }
         </ScrollView>
     );
 };
@@ -84,7 +85,7 @@ const GetInstruction = (props) => {
             </View>
 
 
-            <TouchableOpacity onPress={() =>props.setConfirm(true)}>
+            <TouchableOpacity onPress={() => props.setConfirm(true)}>
                 <View style={styles.view}>
                     <Text style={styles.viewtext}>SEND INSTRUCTIONS</Text>
                 </View>
@@ -96,7 +97,43 @@ const GetInstruction = (props) => {
 const ConfirmMessage = () => {
     return (
         <View>
-            <Text>Confirm Message</Text>
+            <ScrollView>
+                <View style={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: 100,
+                }}>
+                    <AntDesign name="checkcircle" size={100} color="red" />
+                    <Text style={{
+                        fontSize: 30,
+                        fontWeight: "bold",
+                        marginTop: 20
+                    }}>Check your mail</Text>
+                    <Text style={{
+                        fontSize: 20,
+                        color: 'rgb(100,100,100)',
+                    }}>We have sent a password recover</Text>
+                    <Text style={{
+                        fontSize: 20,
+                        color: 'rgb(100,100,100)',
+                    }}>instructions to your email.</Text>
+                </View>
+
+                <View style={{
+                    marginTop: 180,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                        <Text style={{
+                            fontSize:20,
+                        }}>Did not receive the email?</Text>
+                        <Text style={{
+                            fontSize:20,
+                            color:'red',
+                        }}>Check your spam folder.</Text>
+                </View>
+            </ScrollView>
+
         </View>
     )
 }
