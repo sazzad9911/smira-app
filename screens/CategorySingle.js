781c848ebf2e85f396200a9d542d17655f845b00
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-03-16 22:07:00 +0000

    test

--- screens/CategorySingle.js
+++ screens/CategorySingle.js
@@ -3,32 +3,64 @@ import { Ionicons } from '@expo/vector-icons';
 import Cards from '../components/Cards'
 import { View, Text, StyleSheet, Dimensions, ScrollView } from 'react-native'
 import picture from '../assets/tub.png'
+import DealCart from '../components/DealCart'
 
 const CategorySingle = (props) => {
     const title = props.route.params.title;
+    //console.log(title)
 
-    return (
-        <View style={styles.body}>
-            <View style={styles.content}>
-                <Ionicons name="md-checkmark-done-circle-outline" size={20} color='rgba(100, 182, 87, 1)' />
-                <Text style={{
-                    fontSize: 14,
-                    color: '#000000',
-                    fontWeight: '500',
-                }}>
-                    Free for Members
-                </Text>
-            </View>
-            <ScrollView>
-                <View >
-                    <Cards navigation={props.navigation} img={picture} title="On the go"
-                        address="Alibaug, Maharashtra" />
-                    <Cards navigation={props.navigation} img={picture} title="On the go"
-                        address="Alibaug, Maharashtra" />
+    if (title == 'Popular Hotel') {
+        return (
+            <View style={styles.body}>
+                <View style={styles.content}>
+                    <Ionicons name="md-checkmark-done-circle-outline" size={20} color='rgba(100, 182, 87, 1)' />
+                    <Text style={{
+                        fontSize: 14,
+                        color: '#000000',
+                        fontWeight: '500',
+                    }}>
+                        Free for Members
+                    </Text>
                 </View>
-            </ScrollView>
-        </View>
-    );
+                <ScrollView>
+                    <View >
+                        <Cards navigation={props.navigation} img={picture} title="On the go"
+                            address="Alibaug, Maharashtra" />
+                        <Cards navigation={props.navigation} img={picture} title="On the go"
+                            address="Alibaug, Maharashtra" />
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    }
+    else if (title == 'Restuarant') {
+        return (
+            <View style={styles.body}>
+                <View style={styles.content}>
+                    <Ionicons name="md-checkmark-done-circle-outline" size={20} color='rgba(100, 182, 87, 1)' />
+                    <Text style={{
+                        fontSize: 14,
+                        color: '#000000',
+                        fontWeight: '500',
+                    }}>
+                        Free for Members
+                    </Text>
+                </View>
+                <ScrollView>
+                    <View >
+                        <DealCart headLine='Flat 35% OFF On All Order'
+                            category='Ovenstory' img='https://www.daily-sun.com/assets/news_images/2019/09/23/Dailysun-2019-04-22-14.jpg'
+                        />
+                    </View>
+                </ScrollView>
+            </View>
+        );
+    }
+    else {
+        return (
+            <View></View>
+        );
+    };
 };
 
 export default CategorySingle;
