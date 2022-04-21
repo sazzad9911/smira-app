af577ed40994f74b964deab69dfe418583469f55
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-04-21 07:53:29 +0600

    drawer

--- screens/MemberShipInfo.js
+++ screens/MemberShipInfo.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { Image, StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 import userImage from '../assets/10.jpg';
 import { Entypo, Ionicons } from '@expo/vector-icons';
@@ -6,15 +6,27 @@ import { useSelector } from 'react-redux'
 
 const MemberShipInfo = ({ navigation }) => {
   const user = useSelector(state => state.user[0])
-  const Months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  const total=(new Date(user.ending_date).getTime() -new Date(user.starting_date).getTime())/(1000 * 3600 * 24);
-  const reminding=(new Date(user.ending_date).getTime() -new Date().getTime())/(1000 * 3600 * 24);
-  const progress=(reminding*100)/total;
+  const Months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const total = (new Date(user.ending_date).getTime() - new Date(user.starting_date).getTime()) / (1000 * 3600 * 24);
+  const reminding = (new Date(user.ending_date).getTime() - new Date().getTime()) / (1000 * 3600 * 24);
+  const progress = (reminding * 100) / total;
+  const [color,setColor] =React.useState({
+    foreground:'#FC444B',
+    background:'rgba(255, 0, 0, 0.178)',
+  })
+  React.useEffect(()=>{
+    if(user.membership_type=='gold'){
+      setColor({
+        foreground:'#F3B038',
+        background:'#f3b2385b'
+      })
+    }
+  },[])
   return (
-    <View style={{
-      flexDirection: 'column',
-      flex: 1
+    <ScrollView style={{
+      backgroundColor: '#FFFF',
     }}>
+      <View style={{ height: 20 }} />
       <View style={{
         flex: 2,
         width: '100%',
@@ -28,11 +40,14 @@ const MemberShipInfo = ({ navigation }) => {
           marginBottom: 10
         }} />
         <Text style={{
-          margin: 7
+          margin: 7,
+          fontFamily: 'PlusJakartaSans',
+          fontSize: 14
         }}>Hi, {user.name}</Text>
         <Text style={{
           fontWeight: '600',
-          fontSize: 25
+          fontSize: 23,
+          fontFamily: 'PlusJakartaSansBold',
         }}>{
             user.membership_type == 'gold' ?
               (<Text style={{ color: '#F3B038' }}>Gold</Text>) :
@@ -45,30 +60,74 @@ const MemberShipInfo = ({ navigation }) => {
                     (<Text style={{ color: '#000' }}>Non</Text>)
           } Membership</Text>
         <Text style={{ color: 'gray', fontSize: 13, marginTop: 6 }}>Valid up to {new Date(user.ending_date).getDate()
-        +' '+Months[new Date(user.ending_date).getMonth()]+' '+new Date(user.ending_date).getFullYear()}</Text>
-        <Text style={{ marginTop: 15, fontSize: 15 }}><Text style={{ fontWeight: '600' }}>{reminding.toFixed(0)} days left</Text> in membership</Text>
-        <View style={{ borderWidth: 1, width: '80%', marginTop: 15, borderColor: '#FEF893', backgroundColor: '#FEF893' }}>
-          <View style={{ borderWidth: 1, width: progress+'%', borderColor: '#F3B038' }}>
+          + ' ' + Months[new Date(user.ending_date).getMonth()] + ' ' + new Date(user.ending_date).getFullYear()}</Text>
+        <Text style={{
+          marginTop: 15,
+          fontSize: 16,
+          fontFamily: 'PlusJakartaSans'
+        }}>
+          <Text style={{ fontFamily: 'PlusJakartaSansBold' }}>{reminding.toFixed(0)}
+            days left</Text> in membership</Text>
+        <View style={{
+          borderWidth: 1,
+          width: '80%', marginTop: 15,
+          borderColor: color.background,
+          backgroundColor: color.background,
+          borderRadius:2
+        }}>
+          <View style={{
+            borderWidth: 1,
+            width: progress + '%',
+            borderColor: color.foreground
+          }}>
 
           </View>
         </View>
       </View>
       <View style={{ flex: 3, width: '100%', flexDirection: 'column', alignItems: 'center', padding: 5 }}>
-        <Text style={{ fontSize: 17, color: 'gray' }}>Renew your membership</Text>
+        <Text style={{
+          fontSize: 17,
+          color: 'gray',
+          marginTop: 30,
+          marginBottom: 10,
+          fontFamily: 'PlusJakartaSans'
+        }}>Renew your membership</Text>
         <View style={{
-          minHeight: 100, width: '95%', backgroundColor: '#FEF8EC', borderRadius: 10,
+          minHeight: 100, width: '95%', backgroundColor: color.background, borderRadius: 10,
           marginTop: 20, flexDirection: 'row', alignItems: 'center'
         }}>
           <View style={{ flex: 1, height: '100%' }}>
-            <Ionicons name="checkmark-circle" size={24} color="#F3B038" style={{ marginTop: 20, marginLeft: 10 }} />
+            <Ionicons name="checkmark-circle" size={24}
+              color={color.foreground} style={{
+                marginTop: 20,
+                marginLeft: 10
+              }} />
           </View>
           <View style={{ flex: 4, }}>
-            <Text style={{ fontWeight: '500', marginBottom: 6 }}>Continue as <Text style={{ fontWeight: '700', }}>Gold Member</Text></Text>
-            <Text style={{ color: 'gray', fontSize: 12 }}>Special prices are available only till 23 December 2026.</Text>
+            <Text style={{
+              fontWeight: '500',
+              marginBottom: 6,
+              fontFamily: 'PlusJakartaSans'
+            }}>Continue as <Text style={{ fontFamily: 'PlusJakartaSansBold' }}>Gold Member</Text></Text>
+            <Text style={{
+              color: 'gray',
+              fontSize: 12,
+              fontFamily: 'PlusJakartaSans'
+            }}>Special prices are available only till 23 December 2026.</Text>
           </View>
           <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 17, color: '#F3CD74', textDecorationLine: 'line-through', fontWeight: 'bold' }}>₹ 2999</Text>
-            <Text style={{ fontSize: 17, color: '#F3B038', fontWeight: 'bold' }}>₹ 2499</Text>
+            <Text style={{
+              fontSize: 17,
+              color: color.foreground,
+              textDecorationLine: 'line-through',
+              fontFamily: 'PlusJakartaSansBold',
+              opacity: 0.4
+            }}>₹ 2999</Text>
+            <Text style={{
+              fontSize: 17,
+              color: color.foreground,
+              fontFamily: 'PlusJakartaSansBold'
+            }}>₹ 2499</Text>
           </View>
         </View>
 
@@ -103,11 +162,11 @@ const MemberShipInfo = ({ navigation }) => {
           </View>
         </TouchableOpacity>
         <View style={{ alignItems: 'center', justifyContent: 'center', marginTop: 20 }}>
-          <Text style={{ fontSize: 16, color:'#585858'}}>Have a family code?</Text>
+          <Text style={{ fontSize: 16, color: '#585858' }}>Have a family code?</Text>
           <Text style={{ color: '#FC444B', textDecorationLine: 'underline', fontSize: 16, marginTop: 10 }}>Apply it here</Text>
         </View>
       </View>
-    </View>
+    </ScrollView>
   )
 }
 
